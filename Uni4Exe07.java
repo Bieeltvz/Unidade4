8058b90db7639be35253a98a33c4221cee889556
Bieeltvz <110978689+Bieeltvz@users.noreply.github.com>
2024-09-10 21:14:14 -0300

    Add files via upload

---
+++ Uni4Exe07.java
@@ -0,0 +1,24 @@
+package Unidade4;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Uni4Exe07 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+        System.out.println("Entre com peso da carta: ");
+        double pesoCarta = teclado.nextDouble();
+        double valorPagar = 0;
+        if(pesoCarta >= 50 ){
+            double pesoExedido = pesoCarta - 50;
+            double pesoAdicional = (pesoExedido / 20) + 1;
+            valorPagar = 0.45 + 0.45 * pesoAdicional;
+            System.out.println("Custo do selo: " + df.format(valorPagar));
+        } else {
+            valorPagar = 0.45;
+        }
+        System.out.println("Custo do selo " + df.format(valorPagar));
+        teclado.close();
+    }
+    
+}

a6eda77228b1e89239caa581eace2993c44efd51
Bieeltvz <110978689+Bieeltvz@users.noreply.github.com>
2024-09-16 18:58:29 -0300

    Add files via upload

--- Uni4Exe06.java
+++ Uni4Exe06.java
@@ -6,6 +6,7 @@ public class Uni4Exe06 {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Digite M, F ou I");
         String sexo = teclado.nextLine().toUpperCase();//teclado.next().toUpperCase().charAt(0); // toUpperCase pra caso se o usuario digitar letra minuscula O comando charAt(0) é usado em combinação com o comando next() simples , que instrui o Java a registrar o próximo caractere ou string inserido na linha de comando.
+       //.charAT(0) para char Character.toUpperCase.teclado.next().charAT(0) = COM CHAR
         if (sexo.equals("M")){
             System.out.println("Masculino");//Em Java, a comparação de strings deve ser feita com o método .equals() em vez do operador ==.
         } else if (sexo.equals("F")){
